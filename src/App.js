9b426953bc1cdf92457d5f4067c5d3243aeb68e8
sonishalini <shalinisoni2510@gmail.com>
2021-04-23 05:49:37 -0700

    Header Component is Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css"; //inject
 import Header from "./MyComponents/Header";
 import { Todos } from "./MyComponents/Todos";
 import { Footer } from "./MyComponents/Footer";
 
 function App() {
-  let myVariable = 345;
   return (
     <>
-      <Header />
+      {/* <Header title="MyTodosList" searchBar={false} /> */}
+      <Header title="MyTodosList" />
       <Todos />
       <Footer />
     </>
--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,12 +1,13 @@
 //rfc (react function based component)
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Todos-List
+          {props.title}
         </a>
         <button
           className="navbar-toggler"
@@ -32,19 +33,33 @@ export default function Header() {
               </a>
             </li>
           </ul>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+          {props.searchBar ? (
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </nav>
   );
 }
+
+Header.defaultProps = {
+  title: "your title here!",
+  searchBar: true,
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+  searchBar: PropTypes.bool.isRequired,
+};
