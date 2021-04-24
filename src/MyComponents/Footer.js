72d381107f605e40126d32538e13d80e4e0bbc53
sonishalini <shalinisoni2510@gmail.com>
2021-04-24 04:02:26 -0700

    Footer completed

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 export const Footer = () => {
-  //   return <div></div>;
-  return <div>Footer works!</div>;
+  let footerStyle = {
+    position: "absolute",
+    top: "100vh",
+    width: "100%",
+    // border: "2px solid red",
+  };
+  return (
+    <footer className="bg-dark text-light py-3" style={footerStyle}>
+      <p className="text-center">Copyright &copy; MyTodosList.com</p>
+    </footer>
+  );
 };
